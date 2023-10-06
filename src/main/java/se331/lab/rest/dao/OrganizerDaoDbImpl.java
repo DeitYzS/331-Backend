b84b5b03597f6504a56104409691e0a9b321ec83
deityzs <kongkonghihi@gmail.com>
2023-10-06 12:45:37 +0700

    Checkpoint 6.1 (Lab09 - StackOverflowError)

--- src/main/java/se331/lab/rest/dao/OrganizerDaoDbImpl.java
+++ src/main/java/se331/lab/rest/dao/OrganizerDaoDbImpl.java
@@ -4,6 +4,7 @@
 //import org.springframework.context.annotation.Profile;
 //import org.springframework.data.domain.Page;
 //import org.springframework.data.domain.PageRequest;
+//import org.springframework.data.domain.Pageable;
 //import org.springframework.stereotype.Repository;
 //import se331.lab.rest.entity.Organizer;
 //import se331.lab.rest.repository.OrganizerRepository;
@@ -14,23 +15,10 @@
 //@Profile("db")
 //public class OrganizerDaoDbImpl implements OrganizerDao {
 //    final OrganizerRepository organizerRepository ;
-//    @Override
-//    public Integer getOrganizerSize() {
-//        return Math.toIntExact(organizerRepository.count());
-//    }
 //
 //    @Override
-//    public Page<Organizer> getOrganizers(Integer pageSize, Integer page) {
-//        return organizerRepository.findAll(PageRequest.of(page - 1, pageSize));
+//    public Page<Organizer> getOrganizer(Pageable pageRequest) {
+//        return organizerRepository.findAll(pageRequest);
 //    }
 //
-//    @Override
-//    public Organizer getOrganizer(Long id) {
-//        return organizerRepository.findById(id).orElse(null);
-//    }
-//
-//    @Override
-//    public Organizer save(Organizer organizer) {
-//        return organizerRepository.save(organizer);
-//    }
 //}
